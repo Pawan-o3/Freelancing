5398f3821c66da119c8f771ad8e5ff4f8883db1a
Pawan Kumar <157190294+Pawan-o3@users.noreply.github.com>
2025-11-05 19:35:19 +0530

    Refactor GeneralContext for improved login and register

--- client/src/context/GeneralContext.jsx
+++ client/src/context/GeneralContext.jsx
@@ -1,16 +1,13 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import socketIoClient from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 export const GeneralContext = createContext();
 
-const GeneralContextProvider = ({children}) => {
-
+const GeneralContextProvider = ({ children }) => {
   const WS = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5500';
-
-  const socket = socketIoClient(WS);
-
+  const socket = io(WS);
 
   const navigate = useNavigate();
 
@@ -18,82 +15,69 @@ const GeneralContextProvider = ({children}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [usertype, setUsertype] = useState('');
- 
-  
-  
-  
-  const login = async () =>{
-    try{
-      const loginInputs = {email, password}
-        await axios.post(`${WS}/login`, loginInputs)
-        .then( async (res)=>{
 
-          localStorage.setItem('userId', res.data._id);
-            localStorage.setItem('usertype', res.data.usertype);
-            localStorage.setItem('username', res.data.username);
-            localStorage.setItem('email', res.data.email);
-            if(res.data.usertype === 'freelancer'){
-                navigate('/freelancer');
-            } else if(res.data.usertype === 'client'){
-              navigate('/client');
-            } else if(res.data.usertype === 'admin'){
-                navigate('/admin');
-            }
-          }).catch((err) =>{
-            alert("login failed!!");
-            console.log(err);
-          });
-          
-        }catch(err){
-          console.log(err);
-        }
-      }
-      
-  const inputs = {username, email, usertype, password};
+  const login = async () => {
+    try {
+      const loginInputs = { email, password };
+      const res = await axios.post(`${WS}/login`, loginInputs);
+      localStorage.setItem('userId', res.data._id);
+      localStorage.setItem('usertype', res.data.usertype);
+      localStorage.setItem('username', res.data.username);
+      localStorage.setItem('email', res.data.email);
 
-  const register = async () =>{
-    try{
-        await axios.post(`${WS}/register`, inputs)
-        .then( async (res)=>{
-            localStorage.setItem('userId', res.data._id);
-            localStorage.setItem('usertype', res.data.usertype);
-            localStorage.setItem('username', res.data.username);
-            localStorage.setItem('email', res.data.email);
-
-            if(res.data.usertype === 'freelancer'){
-              navigate('/freelancer');
-          } else if(res.data.usertype === 'client'){
-            navigate('/client');
-          } else if(res.data.usertype === 'admin'){
-              navigate('/admin');
-          }
- 
-        }).catch((err) =>{
-            alert("registration failed!!");
-            console.log(err);
-        });
-    }catch(err){
-        console.log(err);
+      if (res.data.usertype === 'freelancer') navigate('/freelancer');
+      else if (res.data.usertype === 'client') navigate('/client');
+      else if (res.data.usertype === 'admin') navigate('/admin');
+    } catch (err) {
+      alert("Login failed!");
+      console.error(err);
     }
-  }
+  };
 
+  const register = async () => {
+    try {
+      const inputs = { username, email, usertype, password };
+      const res = await axios.post(`${WS}/register`, inputs);
 
-  const logout = async () =>{
-    
+      localStorage.setItem('userId', res.data._id);
+      localStorage.setItem('usertype', res.data.usertype);
+      localStorage.setItem('username', res.data.username);
+      localStorage.setItem('email', res.data.email);
+
+      if (res.data.usertype === 'freelancer') navigate('/freelancer');
+      else if (res.data.usertype === 'client') navigate('/client');
+      else if (res.data.usertype === 'admin') navigate('/admin');
+    } catch (err) {
+      alert("Registration failed!");
+      console.error(err);
+    }
+  };
+
+  const logout = () => {
     localStorage.clear();
-    for (let key in localStorage) {
-      if (localStorage.hasOwnProperty(key)) {
-        localStorage.removeItem(key);
-      }
-    }
-    
     navigate('/');
-  }
-
+  };
 
   return (
-    <GeneralContext.Provider value={{socket, login, register, logout, username, setUsername, email, setEmail, password, setPassword, usertype, setUsertype}} >{children}</GeneralContext.Provider>
-  )
-}
+    <GeneralContext.Provider
+      value={{
+        socket,
+        login,
+        register,
+        logout,
+        username,
+        setUsername,
+        email,
+        setEmail,
+        password,
+        setPassword,
+        usertype,
+        setUsertype,
+      }}
+    >
+      {children}
+    </GeneralContext.Provider>
+  );
+};
 
-export default GeneralContextProvider
+export default GeneralContextProvider;
